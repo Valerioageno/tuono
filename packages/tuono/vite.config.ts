04cda909438974e944268fe3486751c66d3751a8
Valerio Ageno <valerioageno@yahoo.it>
2024-05-02 19:18:25 +0200

    feat: create tuono package

---
+++ packages/tuono/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig, mergeConfig } from 'vitest/config'
+import { tanstackBuildConfig } from '@tanstack/config/build'
+import react from '@vitejs/plugin-react'
+
+const config = defineConfig({
+  plugins: [react()],
+  test: {
+    name: 'react-router',
+    watch: false,
+    environment: 'jsdom',
+  },
+})
+
+export default mergeConfig(
+  config,
+  tanstackBuildConfig({
+    entry: './src/index.ts',
+    srcDir: './src',
+  }),
+)
