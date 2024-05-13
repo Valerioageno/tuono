65f43b3cafe55a82ac09d42420aba5ac7f3bc7cc
Valerio Ageno <valerioageno@yahoo.it>
2024-05-13 21:11:22 +0200

    feat: spawn vite from tuono crate

--- crates/cli/src/actions/dev.rs
+++ crates/cli/src/actions/dev.rs
@@ -1,3 +1,24 @@
+use std::path::Path;
+use std::process::{Command, Stdio};
+
+fn spawn_vite_process() -> std::io::Result<()> {
+    println!("Spawing vite process");
+    let current_dir = std::env::current_dir()?;
+
+    let vite_path = Path::new("node_modules/.bin/vite");
+    let vite = current_dir.join(vite_path);
+
+    dbg!(&vite);
+
+    let _ = Command::new(vite)
+        .arg("dev")
+        .stdout(Stdio::inherit())
+        .output()?;
+
+    Ok(())
+}
+
 pub fn run() {
     println!("Running dev environment");
+    let _ = spawn_vite_process();
 }
