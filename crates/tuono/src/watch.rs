8728499f9052b71c6c3503eac9bdf10834191dd8
Valerio Ageno <valerioageno@yahoo.it>
2024-06-08 12:42:58 +0200

    feat: refined reload logs

--- crates/tuono/src/watch.rs
+++ crates/tuono/src/watch.rs
@@ -57,20 +57,28 @@ pub async fn watch() -> Result<()> {
     println!("\nDevelopment app ready at http://localhost:3000/");
 
     let wx = Watchexec::new(move |mut action| {
+        let mut should_reload = false;
+
         for event in action.events.iter() {
             for path in event.paths() {
                 if path.0.to_string_lossy().ends_with(".rs")
                     // Either tsx and jsx
                     || path.0.to_string_lossy().ends_with("sx")
                 {
-                    println!("Reloading...");
-                    run_server.stop();
-                    build_ssr_bundle.stop();
-                    build_ssr_bundle.start();
-                    bundle_axum_source().expect("Failed to bunlde rust source");
-                    run_server.start();
+                    should_reload = true
                 }
             }
+        }
+
+        if should_reload {
+            println!("Reloading...");
+            run_server.stop();
+            build_ssr_bundle.stop();
+            build_ssr_bundle.start();
+            bundle_axum_source().expect("Failed to bunlde rust source");
+            run_server.start();
+            println!("Ready!");
+            should_reload = false;
         }
 
         // if Ctrl-C is received, quit
