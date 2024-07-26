1c2cc71bc84402f5ab65aa32d27a775d3e8dc9f5
Valerio Ageno <valerioageno@yahoo.it>
2024-07-26 16:23:08 +0200

    fix: remove HTMLProps type

--- apps/documentation/src/components/mdx-provider/mdx-link/mdx-link.tsx
+++ apps/documentation/src/components/mdx-provider/mdx-link/mdx-link.tsx
@@ -1,8 +1,8 @@
-import type { HTMLProps } from 'react'
+import type { AnchorHTMLAttributes } from 'react'
 import { Link } from 'tuono'
 
 export default function MdxLink(
-  props: HTMLProps<HTMLAnchorElement>,
+  props: AnchorHTMLAttributes<HTMLAnchorElement>,
 ): JSX.Element {
   if (props.href?.startsWith('http')) {
     return <a {...props} target="_blank" />
