b2940d9fa8b2ee93d2baf0651bccd634e8c83e7d
Valerio Ageno <valerioageno@yahoo.it>
2024-08-15 18:52:56 +0200

    feat: add breadcrumbs to documentation

---
+++ apps/documentation/src/components/breadcrumbs/breadcrumbs.tsx
@@ -0,0 +1,46 @@
+import { Breadcrumbs, Button } from '@mantine/core'
+import { Link } from 'tuono'
+
+import { IconChevronRight, IconBolt } from '@tabler/icons-react'
+import type { ReactNode } from 'react'
+
+export default function TuonoBreadcrumbs({
+  children,
+}: {
+  children: ReactNode
+}): JSX.Element {
+  return (
+    <Breadcrumbs
+      separator={<IconChevronRight size="1.1rem" stroke={1.5} />}
+      mb="md"
+      mt="md"
+    >
+      <Button href="/" component={Link} variant="subtle" radius="xl" p={5}>
+        <IconBolt />
+      </Button>
+      {children}
+    </Breadcrumbs>
+  )
+}
+
+interface BreadcrumbElementProps {
+  href?: string
+  label: string
+}
+
+export function BreadcrumbElement({
+  href,
+  label,
+}: BreadcrumbElementProps): JSX.Element {
+  return (
+    <Button
+      component={href ? Link : 'span'}
+      href={href}
+      variant={href ? 'subtle' : 'light'}
+      radius="xl"
+      px={href ? 10 : undefined}
+    >
+      {label}
+    </Button>
+  )
+}
---
+++ apps/documentation/src/components/breadcrumbs/index.ts
@@ -0,0 +1,5 @@
+import Breadcrumbs, { BreadcrumbElement } from './breadcrumbs'
+
+export default Breadcrumbs
+
+export { BreadcrumbElement as Element }
