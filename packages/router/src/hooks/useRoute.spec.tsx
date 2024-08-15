1b1e91b6c0a3e24883b0e2c3772145809748421d
Valerio Ageno <51341197+Valerioageno@users.noreply.github.com>
2024-08-15 20:42:47 +0200

    fix: sanitize client side pathnames (#25)

--- packages/router/src/hooks/useRoute.spec.tsx
+++ packages/router/src/hooks/useRoute.spec.tsx
@@ -14,7 +14,7 @@ describe('Test useRoute fn', () => {
           routesById: {
             '/': { id: '/' },
             '/about': { id: '/about' },
-            '/posts/': { id: '/posts/' }, // posts/index
+            '/posts': { id: '/posts' }, // posts/index
             '/posts/[post]': { id: '/posts/[post]' },
             '/posts/defined-post': { id: '/posts/defined-post' },
             '/posts/[post]/[comment]': { id: '/posts/[post]/[comment]' },
@@ -26,7 +26,7 @@ describe('Test useRoute fn', () => {
     expect(useRoute('/')?.id).toBe('/')
     expect(useRoute('/not-found')?.id).toBe(undefined)
     expect(useRoute('/about')?.id).toBe('/about')
-    expect(useRoute('/posts/')?.id).toBe('/posts/')
+    expect(useRoute('/posts/')?.id).toBe('/posts')
     expect(useRoute('/posts/dynamic-post')?.id).toBe('/posts/[post]')
     expect(useRoute('/posts/defined-post')?.id).toBe('/posts/defined-post')
     expect(useRoute('/posts/dynamic-post/dynamic-comment')?.id).toBe(
