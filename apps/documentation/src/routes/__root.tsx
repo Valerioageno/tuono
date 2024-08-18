3772e7307e857c8d27ad00d4b934e512fe6f9987
Valerio Ageno <valerioageno@yahoo.it>
2024-08-18 11:32:57 +0200

    feat: add favicon to documentation website

--- apps/documentation/src/routes/__root.tsx
+++ apps/documentation/src/routes/__root.tsx
@@ -66,6 +66,26 @@ export default function RootRoute({ children }: RootRouteProps): JSX.Element {
   return (
     <>
       <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <ColorSchemeScript />
       </Head>
       <MantineProvider theme={theme}>
