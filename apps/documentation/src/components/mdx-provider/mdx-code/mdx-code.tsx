15e568594d2aabda4fa2d07be46408e27540488c
Valerio Ageno <valerio.ageno@qonto.com>
2024-10-17 09:03:05 +0200

    feat: update code font size

--- apps/documentation/src/components/mdx-provider/mdx-code/mdx-code.tsx
+++ apps/documentation/src/components/mdx-provider/mdx-code/mdx-code.tsx
@@ -4,5 +4,5 @@ import type { HTMLAttributes } from 'react'
 export default function MdxCode(
   props: HTMLAttributes<HTMLPreElement>,
 ): JSX.Element {
-  return <Code {...props} />
+  return <Code {...props} style={{ fontSize: 14 }} />
 }
