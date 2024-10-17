b67a8e853785635ec46f7cc8bf7bc45374c493b9
Valerio Ageno <valerio.ageno@qonto.com>
2024-10-17 12:14:18 +0200

    fix: remove table of contents

--- apps/documentation/src/routes/documentation/__root.tsx
+++ apps/documentation/src/routes/documentation/__root.tsx
@@ -2,24 +2,20 @@ import type { ReactNode } from 'react'
 import { AppShell, Container } from '@mantine/core'
 import MdxProvider from '../../components/mdx-provider'
 import EditPage from '../../components/edit-page'
-import TableOfContents from '../../components/table-of-content'
 
 interface RootRouteProps {
-  children: ReactNode
+	children: ReactNode
 }
 
 export default function RootRoute({ children }: RootRouteProps): JSX.Element {
-  return (
-    <AppShell.Main>
-      <Container size="lg" p={20} display={{ lg: 'flex' }}>
-        <Container id="mdx-root" component="article" size="md" p={0}>
-          <MdxProvider>{children}</MdxProvider>
-          <EditPage />
-        </Container>
-        <Container size="xs" visibleFrom="lg" p={0}>
-          <TableOfContents withTabs={false} />
-        </Container>
-      </Container>
-    </AppShell.Main>
-  )
+	return (
+		<AppShell.Main>
+			<Container size="lg" p={20} display={{ lg: 'flex' }}>
+				<Container id="mdx-root" component="article" size="md" p={0}>
+					<MdxProvider>{children}</MdxProvider>
+					<EditPage />
+				</Container>
+			</Container>
+		</AppShell.Main>
+	)
 }
