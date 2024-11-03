9591960e31ffbaf9f86fc15acd841dd76009562a
Valerio Ageno <valerioageno@yahoo.it>
2024-11-03 22:07:22 +0100

    feat: add canonical tags to documentation

--- apps/documentation/src/components/navigation-buttons/navigation-buttons.tsx
+++ apps/documentation/src/components/navigation-buttons/navigation-buttons.tsx
@@ -3,60 +3,60 @@ import { Link } from 'tuono'
 import { IconArrowRight, IconArrowLeft } from '@tabler/icons-react'
 
 interface NavigationButton {
-	href: string
-	title: string
+  href: string
+  title: string
 }
 
 interface NavigationButtonsProps {
-	prev?: NavigationButton
-	next?: NavigationButton
+  prev?: NavigationButton
+  next?: NavigationButton
 }
 
 export default function NavigationButtons({
-	prev,
-	next,
+  prev,
+  next,
 }: NavigationButtonsProps): JSX.Element {
-	return (
-		<Flex mt={50} gap={10}>
-			{prev && <NavigationBtn type="prev" {...prev} />}
-			{next && <NavigationBtn type="next" {...next} />}
-		</Flex>
-	)
+  return (
+    <Flex mt={50} gap={10}>
+      {prev && <NavigationBtn type="prev" {...prev} />}
+      {next && <NavigationBtn type="next" {...next} />}
+    </Flex>
+  )
 }
 
 interface NavigationButtonProps extends NavigationButton {
-	type: 'next' | 'prev'
+  type: 'next' | 'prev'
 }
 
 const NavigationBtn = ({
-	type,
-	title,
-	href,
+  type,
+  title,
+  href,
 }: NavigationButtonProps): JSX.Element => {
-	const heading = type === 'next' ? 'Next' : 'Previous'
-	const textAlign = type === 'next' ? 'left' : 'right'
-	const variant = type === 'next' ? 'filled' : 'outline'
+  const heading = type === 'next' ? 'Next' : 'Previous'
+  const textAlign = type === 'next' ? 'left' : 'right'
+  const variant = type === 'next' ? 'filled' : 'outline'
 
-	return (
-		<Button
-			component={Link}
-			fullWidth
-			variant={variant}
-			href={href}
-			justify="space-between"
-			h="auto"
-			rightSection={type === 'next' && <IconArrowRight />}
-			leftSection={type === 'prev' && <IconArrowLeft />}
-			p="20"
-		>
-			<Box>
-				<Title component="span" display="block" order={4} style={{ textAlign }}>
-					{heading}
-				</Title>
-				<Text component="span" display="block" style={{ textAlign }}>
-					{title}
-				</Text>
-			</Box>
-		</Button>
-	)
+  return (
+    <Button
+      component={Link}
+      fullWidth
+      variant={variant}
+      href={href}
+      justify="space-between"
+      h="auto"
+      rightSection={type === 'next' && <IconArrowRight />}
+      leftSection={type === 'prev' && <IconArrowLeft />}
+      p="20"
+    >
+      <Box>
+        <Title component="span" display="block" order={4} style={{ textAlign }}>
+          {heading}
+        </Title>
+        <Text component="span" display="block" style={{ textAlign }}>
+          {title}
+        </Text>
+      </Box>
+    </Button>
+  )
 }
--- apps/documentation/src/routes/index.tsx
+++ apps/documentation/src/routes/index.tsx
@@ -6,6 +6,7 @@ export default function IndexPage(): JSX.Element {
     <>
       <Head>
         <title>Tuono - The React/Rust full-stack framework</title>
+        <link rel="canonical" href="https://tuono.dev" />
         <meta
           name="description"
           content="The technologies we love seamlessly working together to unleash the highest web performance ever met on React"
