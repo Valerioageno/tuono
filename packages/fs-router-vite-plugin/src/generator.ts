c92981872c818a5c384251a9a20a5600e4b67d55
Valerio Ageno <valerioageno@yahoo.it>
2024-11-04 19:14:39 +0100

    fix: formatting

--- packages/fs-router-vite-plugin/src/generator.ts
+++ packages/fs-router-vite-plugin/src/generator.ts
@@ -4,16 +4,16 @@ import { buildRouteConfig } from './build-route-config'
 import { hasParentRoute } from './has-parent-route'
 
 import {
-	cleanPath,
-	determineNodePath,
-	multiSortBy,
-	replaceBackslash,
-	removeExt,
-	routePathToVariable,
-	removeGroups,
-	removeLastSlash,
-	removeUnderscores,
-	removeLayoutSegments,
+  cleanPath,
+  determineNodePath,
+  multiSortBy,
+  replaceBackslash,
+  removeExt,
+  routePathToVariable,
+  removeGroups,
+  removeLastSlash,
+  removeUnderscores,
+  removeLayoutSegments,
 } from './utils'
 
 import type { Config, RouteNode } from './types'
@@ -26,233 +26,234 @@ import isDefaultExported from './utils/is-default-exported'
 let latestTask = 0
 
 const defaultConfig: Config = {
-	folderName: ROUTES_FOLDER,
-	generatedRouteTree: GENERATED_ROUTE_TREE,
+  folderName: ROUTES_FOLDER,
+  generatedRouteTree: GENERATED_ROUTE_TREE,
 }
 
 let isFirst = false
 let skipMessage = false
 
 async function getRouteNodes(
-	config = defaultConfig,
+  config = defaultConfig,
 ): Promise<{ routeNodes: RouteNode[]; rustHandlersNodes: string[] }> {
-	const routeNodes: RouteNode[] = []
-	const rustHandlersNodes: string[] = []
+  const routeNodes: RouteNode[] = []
+  const rustHandlersNodes: string[] = []
 
-	async function recurse(dir: string): Promise<void> {
-		const fullDir = path.resolve(config.folderName, dir)
-		const dirList = await fsp.readdir(fullDir, { withFileTypes: true })
+  async function recurse(dir: string): Promise<void> {
+    const fullDir = path.resolve(config.folderName, dir)
+    const dirList = await fsp.readdir(fullDir, { withFileTypes: true })
 
-		await Promise.all(
-			dirList.map(async (dirent) => {
-				const fullPath = path.join(fullDir, dirent.name)
-				const relativePath = path.join(dir, dirent.name)
+    await Promise.all(
+      dirList.map(async (dirent) => {
+        const fullPath = path.join(fullDir, dirent.name)
+        const relativePath = path.join(dir, dirent.name)
 
-				if (dirent.isDirectory()) {
-					await recurse(relativePath)
-				} else if (fullPath.match(/\.(tsx|ts|jsx|js|mdx)$/)) {
-					// Check that the route is correctly default exported
-					if (
-						fullPath.match(/\.(tsx|ts|jsx|js)$/) &&
-						!isDefaultExported((await fsp.readFile(fullPath)).toString())
-					) {
-						return
-					}
-					const filePath = replaceBackslash(path.join(dir, dirent.name))
-					const filePathNoExt = removeExt(filePath)
-					let routePath =
-						cleanPath(`/${filePathNoExt.split('.').join('/')}`) || ''
+        if (dirent.isDirectory()) {
+          await recurse(relativePath)
+        } else if (fullPath.match(/\.(tsx|ts|jsx|js|mdx)$/)) {
+          // Check that the route is correctly default exported
+          if (
+            fullPath.match(/\.(tsx|ts|jsx|js)$/) &&
+            !isDefaultExported((await fsp.readFile(fullPath)).toString())
+          ) {
+            return
+          }
+          const filePath = replaceBackslash(path.join(dir, dirent.name))
+          const filePathNoExt = removeExt(filePath)
+          let routePath =
+            cleanPath(`/${filePathNoExt.split('.').join('/')}`) || ''
 
-					const variableName = routePathToVariable(routePath)
+          const variableName = routePathToVariable(routePath)
 
-					// Remove the index from the route path and
-					// if the route path is empty, use `/'
-					if (routePath === 'index') {
-						routePath = '/'
-					}
+          // Remove the index from the route path and
+          // if the route path is empty, use `/'
+          if (routePath === 'index') {
+            routePath = '/'
+          }
 
-					routePath = routePath.replace(/\/index$/, '/') || '/'
+          routePath = routePath.replace(/\/index$/, '/') || '/'
 
-					routeNodes.push({
-						filePath,
-						fullPath,
-						routePath,
-						variableName,
-					})
-				} else if (fullPath.match(/\.(rs)$/)) {
-					const filePath = replaceBackslash(path.join(dir, dirent.name))
-					const filePathNoExt = removeExt(filePath)
-					let routePath =
-						cleanPath(`/${filePathNoExt.split('.').join('/')}`) || ''
+          routeNodes.push({
+            filePath,
+            fullPath,
+            routePath,
+            variableName,
+          })
+        } else if (fullPath.match(/\.(rs)$/)) {
+          const filePath = replaceBackslash(path.join(dir, dirent.name))
+          const filePathNoExt = removeExt(filePath)
+          let routePath =
+            cleanPath(`/${filePathNoExt.split('.').join('/')}`) || ''
 
-					if (routePath === 'index') {
-						routePath = '/'
-					}
+          if (routePath === 'index') {
+            routePath = '/'
+          }
 
-					routePath = routePath.replace(/\/index$/, '/') || '/'
+          routePath = routePath.replace(/\/index$/, '/') || '/'
 
-					rustHandlersNodes.push(routePath)
-				}
-			}),
-		)
-	}
+          rustHandlersNodes.push(routePath)
+        }
+      }),
+    )
+  }
 
-	await recurse('./')
+  await recurse('./')
 
-	return { routeNodes, rustHandlersNodes }
+  return { routeNodes, rustHandlersNodes }
 }
 
 export async function routeGenerator(config = defaultConfig): Promise<void> {
-	if (!isFirst) {
-		isFirst = true
-	} else if (skipMessage) {
-		skipMessage = false
-	}
+  if (!isFirst) {
+    isFirst = true
+  } else if (skipMessage) {
+    skipMessage = false
+  }
 
-	const checkLatest = (): boolean => {
-		if (latestTask !== taskId) {
-			skipMessage = true
-			return false
-		}
+  const checkLatest = (): boolean => {
+    if (latestTask !== taskId) {
+      skipMessage = true
+      return false
+    }
 
-		return true
-	}
+    return true
+  }
 
-	const taskId = latestTask + 1
-	latestTask = taskId
+  const taskId = latestTask + 1
+  latestTask = taskId
 
-	const { routeNodes: beforeRouteNodes, rustHandlersNodes } =
-		await getRouteNodes(config)
+  const { routeNodes: beforeRouteNodes, rustHandlersNodes } =
+    await getRouteNodes(config)
 
-	const preRouteNodes = sortRouteNodes(beforeRouteNodes)
+  const preRouteNodes = sortRouteNodes(beforeRouteNodes)
 
-	const routeNodes: RouteNode[] = []
+  const routeNodes: RouteNode[] = []
 
-	// Loop over the flat list of routeNodes and
-	// build up a tree based on the routeNodes' routePath
-	const handleNode = async (node: RouteNode): Promise<void> => {
-		const parentRoute = hasParentRoute(routeNodes, node, node.routePath)
+  // Loop over the flat list of routeNodes and
+  // build up a tree based on the routeNodes' routePath
+  const handleNode = async (node: RouteNode): Promise<void> => {
+    const parentRoute = hasParentRoute(routeNodes, node, node.routePath)
 
-		if (parentRoute) node.parent = parentRoute
+    if (parentRoute) node.parent = parentRoute
 
-		node.path = determineNodePath(node)
+    node.path = determineNodePath(node)
 
-		node.cleanedPath = removeLastSlash(
-			removeGroups(removeUnderscores(removeLayoutSegments(node.path)) ?? ''),
-		)
+    node.cleanedPath = removeLastSlash(
+      removeGroups(removeUnderscores(removeLayoutSegments(node.path)) ?? ''),
+    )
 
-		if (node.parent) {
-			node.parent.children = node.parent.children ?? []
-			node.parent.children.push(node)
-		}
-		routeNodes.push(node)
-	}
+    if (node.parent) {
+      node.parent.children = node.parent.children ?? []
+      node.parent.children.push(node)
+    }
+    routeNodes.push(node)
+  }
 
-	for (const node of preRouteNodes) {
-		await handleNode(node)
-	}
+  for (const node of preRouteNodes) {
+    await handleNode(node)
+  }
 
-	const routeConfigChildrenText = buildRouteConfig(routeNodes)
+  const routeConfigChildrenText = buildRouteConfig(routeNodes)
 
-	const sortedRouteNodes = multiSortBy(routeNodes, [
-		(d): number => (d.routePath.includes(`/${ROOT_PATH_ID}`) ? -1 : 1),
-		(d): number => d.routePath.split('/').length,
-		(d): number => (d.routePath.endsWith("index'") ? -1 : 1),
-		(d): any => d,
-	])
+  const sortedRouteNodes = multiSortBy(routeNodes, [
+    (d): number => (d.routePath.includes(`/${ROOT_PATH_ID}`) ? -1 : 1),
+    (d): number => d.routePath.split('/').length,
+    (d): number => (d.routePath.endsWith("index'") ? -1 : 1),
+    (d): any => d,
+  ])
 
-	const imports = [
-		...sortedRouteNodes.map((node) => {
-			const extension = node.filePath.endsWith('mdx') ? '.mdx' : ''
-			return `const ${node.variableName
-				}Import = dynamic(() => import('./${replaceBackslash(
-					removeExt(
-						path.relative(
-							path.dirname(config.generatedRouteTree),
-							path.resolve(config.folderName, node.filePath),
-						),
-						false,
-					),
-				)}${extension}'))`
-		}),
-	].join('\n')
+  const imports = [
+    ...sortedRouteNodes.map((node) => {
+      const extension = node.filePath.endsWith('mdx') ? '.mdx' : ''
+      return `const ${
+        node.variableName
+      }Import = dynamic(() => import('./${replaceBackslash(
+        removeExt(
+          path.relative(
+            path.dirname(config.generatedRouteTree),
+            path.resolve(config.folderName, node.filePath),
+          ),
+          false,
+        ),
+      )}${extension}'))`
+    }),
+  ].join('\n')
 
-	const createRoutes = [
-		...sortedRouteNodes.map((node) => {
-			const isRoot = node.routePath.endsWith(ROOT_PATH_ID)
-			const rootDeclaration = isRoot ? ', isRoot: true' : ''
+  const createRoutes = [
+    ...sortedRouteNodes.map((node) => {
+      const isRoot = node.routePath.endsWith(ROOT_PATH_ID)
+      const rootDeclaration = isRoot ? ', isRoot: true' : ''
 
-			return `const ${node.variableName} = createRoute({ component: ${node.variableName}Import${rootDeclaration} })`
-		}),
-	].join('\n')
+      return `const ${node.variableName} = createRoute({ component: ${node.variableName}Import${rootDeclaration} })`
+    }),
+  ].join('\n')
 
-	const createRouteUpdates = [
-		sortedRouteNodes
-			.map((node) => {
-				return [
-					`const ${node.variableName}Route = ${node.variableName}.update({
+  const createRouteUpdates = [
+    sortedRouteNodes
+      .map((node) => {
+        return [
+          `const ${node.variableName}Route = ${node.variableName}.update({
           ${[
-						!node.path?.endsWith(ROOT_PATH_ID) && `path: '${node.cleanedPath}'`,
-						`getParentRoute: () => ${node.parent?.variableName ?? 'root'}Route`,
-						rustHandlersNodes.includes(node.path || '')
-							? 'hasHandler: true'
-							: '',
-					]
-						.filter(Boolean)
-						.join(',')}
+            !node.path?.endsWith(ROOT_PATH_ID) && `path: '${node.cleanedPath}'`,
+            `getParentRoute: () => ${node.parent?.variableName ?? 'root'}Route`,
+            rustHandlersNodes.includes(node.path || '')
+              ? 'hasHandler: true'
+              : '',
+          ]
+            .filter(Boolean)
+            .join(',')}
         })`,
-				].join('')
-			})
-			.join('\n\n'),
-	]
+        ].join('')
+      })
+      .join('\n\n'),
+  ]
 
-	const routeImports = [
-		'// This file is auto-generated by Tuono',
-		"import { createRoute, dynamic } from 'tuono'",
-		[
-			`import RootImport from './${replaceBackslash(
-				path.relative(
-					path.dirname(config.generatedRouteTree),
-					path.resolve(config.folderName, ROOT_PATH_ID),
-				),
-			)}'`,
-		].join('\n'),
-		imports,
-		'const rootRoute = createRoute({ isRoot: true, component: RootImport });',
-		createRoutes,
-		'// Create/Update Routes',
-		createRouteUpdates,
-		'// Create and export the route tree',
-		`export const routeTree = rootRoute.addChildren([${routeConfigChildrenText}])`,
-	]
-		.filter(Boolean)
-		.join('\n\n')
+  const routeImports = [
+    '// This file is auto-generated by Tuono',
+    "import { createRoute, dynamic } from 'tuono'",
+    [
+      `import RootImport from './${replaceBackslash(
+        path.relative(
+          path.dirname(config.generatedRouteTree),
+          path.resolve(config.folderName, ROOT_PATH_ID),
+        ),
+      )}'`,
+    ].join('\n'),
+    imports,
+    'const rootRoute = createRoute({ isRoot: true, component: RootImport });',
+    createRoutes,
+    '// Create/Update Routes',
+    createRouteUpdates,
+    '// Create and export the route tree',
+    `export const routeTree = rootRoute.addChildren([${routeConfigChildrenText}])`,
+  ]
+    .filter(Boolean)
+    .join('\n\n')
 
-	const routeConfigFileContent = await format(routeImports, {
-		semi: false,
-		singleQuote: true,
-		parser: 'typescript',
-	})
+  const routeConfigFileContent = await format(routeImports, {
+    semi: false,
+    singleQuote: true,
+    parser: 'typescript',
+  })
 
-	const routeTreeContent = await fsp
-		.readFile(path.resolve(config.generatedRouteTree), 'utf-8')
-		.catch((err: any) => {
-			if (err.code === 'ENOENT') {
-				return undefined
-			}
-			throw err
-		})
+  const routeTreeContent = await fsp
+    .readFile(path.resolve(config.generatedRouteTree), 'utf-8')
+    .catch((err: any) => {
+      if (err.code === 'ENOENT') {
+        return undefined
+      }
+      throw err
+    })
 
-	if (!checkLatest()) return
+  if (!checkLatest()) return
 
-	if (routeTreeContent !== routeConfigFileContent) {
-		await fsp.mkdir(path.dirname(path.resolve(config.generatedRouteTree)), {
-			recursive: true,
-		})
-		if (!checkLatest()) return
-		await fsp.writeFile(
-			path.resolve(config.generatedRouteTree),
-			routeConfigFileContent,
-		)
-	}
+  if (routeTreeContent !== routeConfigFileContent) {
+    await fsp.mkdir(path.dirname(path.resolve(config.generatedRouteTree)), {
+      recursive: true,
+    })
+    if (!checkLatest()) return
+    await fsp.writeFile(
+      path.resolve(config.generatedRouteTree),
+      routeConfigFileContent,
+    )
+  }
 }
--- packages/fs-router-vite-plugin/src/utils/is-default-exported.ts
+++ packages/fs-router-vite-plugin/src/utils/is-default-exported.ts
@@ -1,4 +1,4 @@
 export default function isDefaultExported(source: string): boolean {
-	const regex = new RegExp('export default')
-	return regex.test(source)
+  const regex = new RegExp('export default')
+  return regex.test(source)
 }
