9f103c48d856598fcae717ee5bb4ca7724ec6c27
Valerio Ageno <valerioageno@yahoo.it>
2024-11-11 21:38:47 +0100

    chore: update tutorial example

--- examples/tutorial/src/routes/pokemons/GOAT.rs
+++ examples/tutorial/src/routes/pokemons/GOAT.rs
@@ -1,7 +1,7 @@
 // src/routes/pokemons/GOAT.rs
-use tuono_lib::{reqwest::Client, Request, Response};
+use tuono_lib::{Request, Response};
 
 #[tuono_lib::handler]
-async fn redirect_to_goat(_: Request, _: Client) -> Response {
+async fn redirect_to_goat(_req: Request) -> Response {
     Response::Redirect("/pokemons/mewtwo".to_string())
 }
