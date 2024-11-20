94b576c20bc7f03fb64cc7cff6df01206356eb65
Valerio Ageno <valerioageno@yahoo.it>
2024-11-20 21:14:50 +0100

    refactor: tuono base health_check

--- examples/tuono/src/routes/api/health_check.rs
+++ examples/tuono/src/routes/api/health_check.rs
@@ -1,11 +1,7 @@
+use tuono_lib::axum::http::StatusCode;
 use tuono_lib::Request;
 
 #[tuono_lib::api(GET)]
-pub async fn my_get_request(_req: Request) -> &'static str {
-    "(GET) Response"
-}
-
-#[tuono_lib::api(POST)]
-pub async fn post_request(_req: Request) -> &'static str {
-    "(POST) Response"
+pub async fn my_get_request(_req: Request) -> StatusCode {
+    StatusCode::OK
 }
