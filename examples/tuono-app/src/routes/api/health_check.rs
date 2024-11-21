d1a5fe234d57a5599cc9307607c64a05e12056c7
Valerio Ageno <valerioageno@yahoo.it>
2024-11-21 19:16:06 +0100

    fix: examples folder

--- examples/tuono-app/src/routes/api/health_check.rs
+++ examples/tuono-app/src/routes/api/health_check.rs
@@ -1,5 +1,5 @@
-use tuono_lib::axum::http::StatusCode;
 use tuono_lib::Request;
+use tuono_lib::axum::http::StatusCode;
 
 #[tuono_lib::api(GET)]
 pub async fn my_get_request(_req: Request) -> StatusCode {
