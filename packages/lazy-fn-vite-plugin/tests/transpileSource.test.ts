4ef7ca7b251b3cd8c15f7e9f31f52f9c673964d6
Marco Pasqualetti <marco.pasqualetti@live.com>
2024-11-24 09:35:35 +0100

    chore(packages/lazy-fn-vite-plugin): make unit test ts compliant

--- packages/lazy-fn-vite-plugin/tests/transpileSource.test.ts
+++ packages/lazy-fn-vite-plugin/tests/transpileSource.test.ts
@@ -1,4 +1,5 @@
 import { it, expect, describe } from 'vitest'
+import type { Plugin } from 'vite'
 import { LazyLoadingPlugin } from '../src'
 
 const SOURCE_CODE = `
@@ -20,9 +21,21 @@ const PokemonspokemonImport = dynamic(
 )
 `
 
+type ViteTransformHandler = Exclude<
+  Plugin['transform'],
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  Function | undefined
+>['handler']
+
+// Create a type-safe transform method
+function getTransform(): (...args: Parameters<ViteTransformHandler>) => string {
+  return LazyLoadingPlugin().transform as never
+}
+
 describe('Transpile tuono source', () => {
   it('Into the client bundle', () => {
-    const bundle = LazyLoadingPlugin().transform?.(SOURCE_CODE, 'id')
+    const pluginTransform = getTransform()
+    const bundle = pluginTransform(SOURCE_CODE, 'id')
     expect(bundle)
       .toBe(`import { createRoute, lazyLoadComponent as dynamic } from 'tuono';
 const IndexImport = dynamic(() => import('./../src/routes/index'));
@@ -30,7 +43,8 @@ const PokemonspokemonImport = dynamic(() => import('./../src/routes/pokemons/[po
   })
 
   it('Into the server bundle', () => {
-    const bundle = LazyLoadingPlugin().transform?.(SOURCE_CODE, 'id', {
+    const pluginTransform = getTransform()
+    const bundle = pluginTransform(SOURCE_CODE, 'id', {
       ssr: true,
     })
     expect(bundle).toBe(`import { createRoute } from 'tuono';
@@ -41,7 +55,8 @@ import PokemonspokemonImport from "./../src/routes/pokemons/[pokemon]";`)
 
 describe('Non tuono dynamic function', () => {
   it('Into the client bundle', () => {
-    const bundle = LazyLoadingPlugin().transform?.(NON_DYNAMIC_SOURCE, 'id')
+    const pluginTransform = getTransform()
+    const bundle = pluginTransform(NON_DYNAMIC_SOURCE, 'id')
     expect(bundle).toBe(`import { createRoute } from 'tuono';
 import { dynamic } from 'external-lib';
 const IndexImport = dynamic(() => import('./../src/routes/index'));
@@ -49,7 +64,8 @@ const PokemonspokemonImport = dynamic(() => import('./../src/routes/pokemons/[po
   })
 
   it('Into the server bundle', () => {
-    const bundle = LazyLoadingPlugin().transform?.(NON_DYNAMIC_SOURCE, 'id', {
+    const pluginTransform = getTransform()
+    const bundle = pluginTransform(NON_DYNAMIC_SOURCE, 'id', {
       ssr: true,
     })
     expect(bundle).toBe(`import { createRoute } from 'tuono';
