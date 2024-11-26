9058f209dd932261256b944ad0117867452ff0fc
Marco Pasqualetti <24919330+marcalexiei@users.noreply.github.com>
2024-11-26 17:50:53 +0100

    ci: add Windows and MacOS to test matrix for both `rust` and `typescript` (#158)

--- packages/fs-router-vite-plugin/tests/generator.spec.ts
+++ packages/fs-router-vite-plugin/tests/generator.spec.ts
@@ -2,40 +2,28 @@ import fs from 'fs/promises'
 import { describe, it, expect } from 'vitest'
 import { routeGenerator } from '../src/generator'
 
-function makeFolderDir(folder: string) {
-  return process.cwd() + `/tests/generator/${folder}`
-}
-
-async function getRouteTreeFileText(folder: string) {
-  const dir = makeFolderDir(folder)
-  return await fs.readFile(dir + '/routeTree.gen.ts', 'utf-8')
-}
-
-async function getExpectedRouteTreeFileText(folder: string) {
-  const dir = makeFolderDir(folder)
-  const location = dir + '/routeTree.expected.ts'
-  return await fs.readFile(location, 'utf-8')
-}
-
 describe('generator works', async () => {
   const folderNames = await fs.readdir(process.cwd() + '/tests/generator')
 
-  it.each(folderNames.map((folder) => [folder]))(
+  it.each(folderNames)(
     'should wire-up the routes for a "%s" tree',
     async (folderName) => {
-      const currentFolder = `${process.cwd()}/tests/generator/${folderName}`
+      const testDirPath = `${process.cwd()}/tests/generator/${folderName}`
 
       await routeGenerator({
-        folderName: `${currentFolder}/routes`,
-        generatedRouteTree: `${currentFolder}/routeTree.gen.ts`,
+        folderName: `${testDirPath}/routes`,
+        generatedRouteTree: `${testDirPath}/routeTree.gen.ts`,
       })
 
-      const [expectedRouteTree, generatedRouteTree] = await Promise.all([
-        getExpectedRouteTreeFileText(folderName),
-        getRouteTreeFileText(folderName),
-      ])
+      const generatedFilePath = `${testDirPath}/routeTree.gen.ts`
+      const expectedFilePath = `${testDirPath}/routeTree.expected.ts`
 
-      expect(generatedRouteTree).equal(expectedRouteTree)
+      const generatedFileContent = await fs.readFile(generatedFilePath, 'utf-8')
+
+      await expect(generatedFileContent).toMatchFileSnapshot(
+        expectedFilePath,
+        `${generatedFilePath} content should be equal to ${expectedFilePath}`,
+      )
     },
   )
 })
