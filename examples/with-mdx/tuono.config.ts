813169f49d4acec696d7ab77f3f666df4f398ff0
Valerio Ageno <valerioageno@yahoo.it>
2024-11-29 21:08:13 +0100

    chore: remove config from examples

--- examples/with-mdx/tuono.config.ts
+++ examples/with-mdx/tuono.config.ts
@@ -1,12 +1,5 @@
 import type { TuonoConfig } from 'tuono/config'
-import { path } from 'tuono/config'
 
-const config: TuonoConfig = {
-	vite: {
-		alias: {
-			'@': path.join(__dirname, './src'),
-		},
-	},
-}
+const config: TuonoConfig = {}
 
 export default config
