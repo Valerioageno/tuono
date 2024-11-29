62c23921f03280d9a2ec4eb5971a5adf166d0383
Valerio Ageno <valerio.ageno@qonto.com>
2024-11-29 11:59:49 +0100

    test: create route integration test

--- crates/tuono/tests/cli_tests.rs
+++ crates/tuono/tests/cli_tests.rs
@@ -1,43 +1,32 @@
-use std::env;
-use std::fs;
 mod utils;
 use assert_cmd::Command;
-use utils::create_temp_tuono_folder;
+use serial_test::serial;
+use std::fs;
+use utils::TempTuonoProject;
 
-const MAIN_RS_CONTENT: &str = "async fn main() {";
-
-// Tests the 'tuono build' command by confirming that main.rs is build and that it contains the start of main() function.
 #[test]
-fn test_tuono_build_success() {
-    let temp_dir = create_temp_tuono_folder();
-    let temp_path = temp_dir.path();
+#[serial]
+fn it_successfully_create_the_index_route() {
+    let temp_tuono_project = TempTuonoProject::new();
 
-    let original_dir = env::current_dir().expect("Failed to get the current directory.");
-    env::set_current_dir(&temp_path).expect("Failed to change the current directory.");
+    temp_tuono_project.add_route("./src/routes/index.rs");
 
-    let test_result = std::panic::catch_unwind(|| {
-        let mut test_tuono_build = Command::cargo_bin("tuono").unwrap();
-        test_tuono_build
-            .arg("build")
-            .arg("--no-js-emit")
-            .assert()
-            .success();
+    let mut test_tuono_build = Command::cargo_bin("tuono").unwrap();
+    test_tuono_build
+        .arg("build")
+        .arg("--no-js-emit")
+        .assert()
+        .success();
 
-        let temp_main_rs_path = temp_path.join(".tuono/main.rs");
-        assert!(
-            temp_main_rs_path.exists(),
-            "'.tuono/main.rs' was not created successfully."
-        );
+    let temp_main_rs_path = temp_tuono_project.path().join(".tuono/main.rs");
 
-        let temp_main_rs_content = fs::read_to_string(&temp_main_rs_path)
-            .expect("Failed to read '.tuono/main.rs' content.");
-        assert!(
-            temp_main_rs_content.contains(MAIN_RS_CONTENT),
-            "'.tuono/main.rs' doesn't contain the expected code."
-        );
-    });
+    let temp_main_rs_content =
+        fs::read_to_string(&temp_main_rs_path).expect("Failed to read '.tuono/main.rs' content.");
 
-    env::set_current_dir(original_dir).expect("Failed to restore the original directory.");
+    assert!(temp_main_rs_content.contains(r#"#[path="../src/routes/index.rs"]"#));
+    assert!(temp_main_rs_content.contains("mod index;"));
 
-    test_result.unwrap()
+    assert!(temp_main_rs_content.contains(
+        r#".route("/", get(index::route)).route("/__tuono/data/data.json", get(index::api))"#
+    ));
 }
