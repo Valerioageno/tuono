576cd4d318d14ffb301e9f4dc8ae8a630c01d707
Marco Pasqualetti <marco.pasqualetti@live.com>
2024-11-30 15:36:55 +0100

    test(packages/tuono): correct test for windows

--- packages/tuono/src/build/utils.spec.ts
+++ packages/tuono/src/build/utils.spec.ts
@@ -1,3 +1,5 @@
+import path from 'node:path'
+
 import { describe, expect, it, vi } from 'vitest'
 
 import type { TuonoConfig } from '../config'
@@ -40,15 +42,13 @@ describe('normalizeConfig - vite - alias', () => {
     const config: TuonoConfig = {
       vite: { alias: { [libraryName]: libraryAlias } },
     }
-    expect(normalizeConfig(config)).toMatchInlineSnapshot(`
-      {
-        "vite": {
-          "alias": {
-            "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
-          },
+    expect(normalizeConfig(config)).toStrictEqual({
+      vite: {
+        alias: {
+          '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
         },
-      }
-    `)
+      },
+    })
   })
 
   it('should transform relative paths to absolute path relative to process.cwd()', () => {
@@ -56,32 +56,28 @@ describe('normalizeConfig - vite - alias', () => {
       vite: { alias: { '@': './src', '@no-prefix': 'src' } },
     }
 
-    expect(normalizeConfig(config)).toMatchInlineSnapshot(`
-      {
-        "vite": {
-          "alias": {
-            "@": "PROCESS_CWD_MOCK/src",
-            "@no-prefix": "PROCESS_CWD_MOCK/src",
-          },
+    expect(normalizeConfig(config)).toStrictEqual({
+      vite: {
+        alias: {
+          '@': path.join(PROCESS_CWD_MOCK, 'src'),
+          '@no-prefix': path.join(PROCESS_CWD_MOCK, 'src'),
         },
-      }
-    `)
+      },
+    })
   })
 
   it('should not transform alias with absolute path', () => {
     const config: TuonoConfig = {
       vite: { alias: { '@1': '/src/pippo', '@2': 'file://pluto' } },
     }
-    expect(normalizeConfig(config)).toMatchInlineSnapshot(`
-      {
-        "vite": {
-          "alias": {
-            "@1": "/src/pippo",
-            "@2": "file://pluto",
-          },
+    expect(normalizeConfig(config)).toStrictEqual({
+      vite: {
+        alias: {
+          '@1': '/src/pippo',
+          '@2': 'file://pluto',
         },
-      }
-    `)
+      },
+    })
   })
 
   it('should apply previuos behaviuor when using alias as list', () => {
@@ -94,25 +90,23 @@ describe('normalizeConfig - vite - alias', () => {
         ],
       },
     }
-    expect(normalizeConfig(config)).toMatchInlineSnapshot(`
-      {
-        "vite": {
-          "alias": [
-            {
-              "find": "1",
-              "replacement": "@tabler/icons-react-fun",
-            },
-            {
-              "find": "2",
-              "replacement": "PROCESS_CWD_MOCK/src",
-            },
-            {
-              "find": "3",
-              "replacement": "file://pluto",
-            },
-          ],
-        },
-      }
-    `)
+    expect(normalizeConfig(config)).toStrictEqual({
+      vite: {
+        alias: [
+          {
+            find: '1',
+            replacement: '@tabler/icons-react-fun',
+          },
+          {
+            find: '2',
+            replacement: path.join(PROCESS_CWD_MOCK, 'src'),
+          },
+          {
+            find: '3',
+            replacement: 'file://pluto',
+          },
+        ],
+      },
+    })
   })
 })
