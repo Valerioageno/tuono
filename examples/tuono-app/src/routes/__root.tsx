d1e7fa064275915d55b918b04168d886401e1941
NayanVR <nvradadiya281@gmail.com>
2024-12-03 15:46:37 +0530

    Add ⚡️ as default favicon for documentation and examples (#191)

--- examples/tuono-app/src/routes/__root.tsx
+++ examples/tuono-app/src/routes/__root.tsx
@@ -1,9 +1,21 @@
 import type { ReactNode, JSX } from 'react'
+import { Head } from 'tuono'
 
 interface RootRouteProps {
   children: ReactNode
 }
 
 export default function RootRoute({ children }: RootRouteProps): JSX.Element {
-  return <main className="main">{children}</main>
+  return (
+    <>
+      <Head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡</text></svg>"
+        />
+        <title>Tuono app</title>
+      </Head>
+      <main className="main">{children}</main>
+    </>
+  )
 }
