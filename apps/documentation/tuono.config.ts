04615ac11cfc862713366ed49d24c110e8b83c68
Marco Pasqualetti <marco.pasqualetti@live.com>
2024-12-09 07:16:24 +0100

    docs: update tuono to 0.16 and add mdx support

--- apps/documentation/tuono.config.ts
+++ apps/documentation/tuono.config.ts
@@ -1,12 +1,19 @@
 import type { TuonoConfig } from 'tuono/config'
+import mdx from '@mdx-js/rollup'
 
 const config: TuonoConfig = {
-	vite: {
-		alias: {
-			'@': 'src',
-			'@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
-		},
-	},
+  vite: {
+    alias: {
+      '@': 'src',
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+    },
+    optimizeDeps: {
+      exclude: ['@mdx-js/react'],
+    },
+    plugins: [
+      { enforce: 'pre', ...mdx({ providerImportSource: '@mdx-js/react' }) },
+    ],
+  },
 }
 
 export default config
