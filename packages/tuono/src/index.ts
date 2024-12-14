767b9fd7121a62ec27d652bbddc612905999dbda
Valerio Ageno <valerioageno@yahoo.it>
2024-12-14 19:07:58 +0100

    fix: formatting

--- packages/tuono/src/index.ts
+++ packages/tuono/src/index.ts
@@ -1,14 +1,14 @@
 export { Helmet as Head } from 'react-helmet-async'
 
 export {
-	createRoute,
-	createRootRoute,
-	createRouter,
-	Link,
-	RouterProvider,
-	dynamic,
-	tuono__internal__lazyLoadComponent,
-	useRouter,
+  createRoute,
+  createRootRoute,
+  createRouter,
+  Link,
+  RouterProvider,
+  dynamic,
+  tuono__internal__lazyLoadComponent,
+  useRouter,
 } from 'tuono-router'
 
 export type { TuonoProps } from './types'
